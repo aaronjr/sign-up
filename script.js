0c50163712d79c60cfd7c70bba27e57afe34d3bb
Aaron Richards <aaron_richards@hotmail.co.uk>
2022-10-20 23:21:00 +0100

    add validaiton of DOB

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
 document.addEventListener('DOMContentLoaded', ()=>{
-
+    // get items from form
     let name = document.querySelector('#name')
     let email = document.querySelector('#email')
     let number = document.querySelector('#number')
-
+    
+    // add to a list
     let inputs = [name, email, number]
 
+    // for each one, add a checker and change background color
     inputs.forEach(element => {
        element.addEventListener('blur', () =>{
             element.checkValidity() ? element.style.borderColor = "green" : element.style.borderColor = "red"
        })
     });
 
+    // get both password fields
     let password = document.querySelector('#password')
     let confirm = document.querySelector('#confirm')
 
+    // add to list
     let pass = [password, confirm]
+
+    // on keyup, check not blank and if match make border green else red. 
     pass.forEach(element => {
         element.addEventListener('keyup',()=>{
             if(element.value !== "" && password.value === confirm.value){
@@ -29,4 +35,19 @@ document.addEventListener('DOMContentLoaded', ()=>{
         })
     })
 
+    // check if user over 18.
+    // get dob input
+    let dob = document.querySelector("#dob")
+
+    // Create date thats 18 years old.
+    date = new Date()
+    let thisMonth = (date.getMonth().length != 1) ? `0${date.getMonth()}` : date.getMonth();
+    thisDay = (date.getDay().length != 1) ? `0${date.getDay()}` : date.getDay()
+    compareYear = date.getFullYear() - 18
+    youngest = `${compareYear}-${thisMonth}-${thisDay}`
+
+    // if input is younger, make border red else green
+    dob.addEventListener('change', ()=>{
+        dob.value > youngest ? dob.style.borderColor = "red" : dob.style.borderColor = "green"
+    })
 })
